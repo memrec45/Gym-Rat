071d93e509fcd6a906c93c82d39e4d81954a6375
memrec <m.emrec45@gmail.com>
2022-12-29 17:07:02 +0300

    day_selector added to the exercise_section. Now I can change the exercise day using butttons.

--- gym_rate/lib/utils/day_selector.dart
+++ gym_rate/lib/utils/day_selector.dart
@@ -4,23 +4,28 @@ import 'package:provider/provider.dart';
 import '../models/exercise_model.dart';
 
 class ExerciseDaySelector extends StatelessWidget {
-  ExerciseDaySelector({Key? key, required this.changeExerciseDay})
+  ExerciseDaySelector(
+      {Key? key, required this.changeExerciseDay, required this.selectedDay})
       : super(key: key);
 
   final Function changeExerciseDay;
-
+  final selectedDay;
   @override
   Widget build(BuildContext context) {
     return Consumer<ExerciseData>(builder: (context, val, child) {
       return Container(
-        height: 50,
+        height: 52,
         child: ListView.builder(
           scrollDirection: Axis.horizontal,
           itemCount: val.dayNames.length,
           itemBuilder: (BuildContext context, int index) {
             String _dayName = val.dayNames.keys.toList()[index];
-            
-            return MyButton(changeExerciseDay: changeExerciseDay, dayName: _dayName);
+
+            return MyButton(
+              changeExerciseDay: changeExerciseDay,
+              dayName: _dayName,
+              selectedDay: selectedDay,
+            );
           },
         ),
       );
@@ -33,38 +38,63 @@ class MyButton extends StatefulWidget {
     Key? key,
     required this.changeExerciseDay,
     required this.dayName,
+    required this.selectedDay,
   }) : super(key: key);
   final Function changeExerciseDay;
   final String dayName;
+  final String selectedDay;
 
   @override
   State<MyButton> createState() => _MyButtonState();
 }
 
 class _MyButtonState extends State<MyButton> {
-  bool a = false;
+  // bool a = false;
 
   @override
   Widget build(BuildContext context) {
     //print(" asdasd : "+dayName);
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 5,vertical:5),
-      child: Container(
-        padding:const EdgeInsets.all(5),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color:a ? Colors.red : Colors.grey[300]
+      padding: const EdgeInsets.symmetric(
+        horizontal: 5,
+        vertical: 5,
+      ),
+      child: GestureDetector(
+        onTap: () {
+          widget.changeExerciseDay(widget.dayName);
+        },
+        child: Container(
+          //height: 150,
+          padding: const EdgeInsets.all(5),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            // color: widget.selectedDay == widget.dayName
+            //     ? Colors.red
+            //     : Colors.grey[300],
+          ),
+          child: Column(
+            children: [
+              Text(
+                widget.dayName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              widget.selectedDay == widget.dayName
+                  ? const SizedBox(
+                      width: 100,
+                      child: Divider(
+                        thickness: 3,
+                        color: Colors.red,
+                        endIndent: 25,
+                        indent: 25,
+                      ),
+                    )
+                  : const SizedBox(),
+            ],
+          ),
         ),
-        child: GestureDetector(
-            onTap: () {
-              setState(() {
-              a = true;
-                
-              });
-              widget.changeExerciseDay(widget.dayName);
-            },
-            child: Text(widget.dayName)),
       ),
     );
   }
--- gym_rate/lib/utils/exercise_section.dart
+++ gym_rate/lib/utils/exercise_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/exercise_model.dart';
+import 'day_selector.dart';
 import 'exercise_tile.dart';
 
 class ExercisesSection extends StatefulWidget {
@@ -23,48 +24,59 @@ class _ExercisesSectionState extends State<ExercisesSection> {
     );
   }
 
+  void changeExerciseDay(String _dayName) {
+    setState(() {
+      selectedDay = _dayName;
+    });
+  }
+
   final _pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
     return Consumer<ExerciseData>(builder: (context, val, child) {
-      return Expanded(
-        child: PageView.builder(
-          itemCount: val.dayNames.length,
-          controller: _pageController,
-          onPageChanged: (pageIndex) => _onPageChanged(val, pageIndex),
-          itemBuilder: (context, index) {
-            //asdasdad
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(
-                      "$selectedDay",
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+      final double maxHeight = MediaQuery.of(context).size.height;
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ExerciseDaySelector(
+            changeExerciseDay: changeExerciseDay,
+            selectedDay: selectedDay,
+          ),
+          const Divider(
+            thickness: 2,
+            height: 5,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Container(
+              decoration: BoxDecoration(color: Colors.grey[200]),
+              height: maxHeight * 0.31,
+              child: PageView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: val.dayNames.length,
+                controller: _pageController,
+                onPageChanged: (pageIndex) => _onPageChanged(val, pageIndex),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: maxHeight * 0.3,
+                          child: ExerciseTile(
+                            selectedDay: selectedDay,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  const Divider(
-                    thickness: 2,
-                    height: 5,
-                  ),
-                  Container(
-                    height: 300,
-                    child: ExerciseTile(
-                      selectedDay: selectedDay,
-                    ),
-                  ),
-                ],
+                  );
+                },
               ),
-            );
-          },
-        ),
+            ),
+          ),
+        ],
       );
     });
   }
