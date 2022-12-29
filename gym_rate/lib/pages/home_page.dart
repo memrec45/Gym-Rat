d8892bae4f8da20e4dcd8502d9c6e189c09d5e83
memrec <m.emrec45@gmail.com>
2022-12-29 08:52:14 +0300

    home_page.dart reorgenized

--- gym_rate/lib/pages/home_page.dart
+++ gym_rate/lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:gym_rate/utils/day_selector.dart';
 import 'package:gym_rate/utils/my_appbar.dart';
 import 'package:provider/provider.dart';
 
+import '../utils/exercise_section.dart';
 import '../utils/exercise_tile.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,18 +15,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String selectedDay = "Volume Day";
-
-  void changeSelectedDay(String name) {
-    setState(() {
-      selectedDay = name;
-    });
-
-    //print(selectedDay);
-  }
-
-  final _pageController = PageController(initialPage: 0);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,63 +28,7 @@ class _HomePageState extends State<HomePage> {
           //selectedDay = val.dayNames.keys.toList()[0];
           return Column(
             children: [
-              Expanded(
-                //height: 300,
-                child: PageView.builder(
-                  itemCount: val.dayNames.length,
-                  controller: _pageController,
-                  onPageChanged: (int pageIndex) {
-                    setState(
-                      () {
-                        selectedDay = val.dayNames.keys.toList()[pageIndex];
-                      },
-                    );
-                  },
-                  itemBuilder: (context, index) {
-                    //asdasdad
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Text(
-                              "$selectedDay",
-                              style:const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          const Divider(
-                            thickness: 2,
-                            height: 5,
-                          ),
-                          Container(
-                            height: 300,
-                            child: ExerciseTile(
-                              selectedDay: selectedDay,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                    // Container(
-                    //   //height: 500,
-                    //   child: Consumer<ExerciseData>(
-                    //     builder: (context, val, child) {
-                    //       return ExerciseTile(
-                    //         selectedDay: selectedDay,
-                    //       );
-                    //     },
-                    //   ),
-                    // );
-                  },
-                ),
-              ),
-
+              ExercisesSection(),
               // ExerciseDaySelector(
               //   changeExerciseDay: changeSelectedDay,
               // ),
--- gym_rate/lib/utils/exercise_section.dart
+++ gym_rate/lib/utils/exercise_section.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ExercisesSection extends StatelessWidget {
-  const ExercisesSection({ Key? key }) : super(key: key);
+import '../models/exercise_model.dart';
+import 'exercise_tile.dart';
+
+class ExercisesSection extends StatefulWidget {
+  ExercisesSection({Key? key}) : super(key: key);
+
+  @override
+  State<ExercisesSection> createState() => _ExercisesSectionState();
+}
+
+class _ExercisesSectionState extends State<ExercisesSection> {
+  String selectedDay = "Volume Day";
+
+  final _pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<ExerciseData>(builder: (context, val, child) {
+      return Expanded(
+        //height: 300,
+        child: PageView.builder(
+          itemCount: val.dayNames.length,
+          controller: _pageController,
+          onPageChanged: (int pageIndex) {
+            setState(
+              () {
+                selectedDay = val.dayNames.keys.toList()[pageIndex];
+              },
+            );
+          },
+          itemBuilder: (context, index) {
+            //asdasdad
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      "$selectedDay",
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const Divider(
+                    thickness: 2,
+                    height: 5,
+                  ),
+                  Container(
+                    height: 300,
+                    child: ExerciseTile(
+                      selectedDay: selectedDay,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      );
+    });
   }
 }
