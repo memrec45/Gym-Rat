8c54e645f842d5196d349a7abb1e8f24f7c32161
memrec <m.emrec45@gmail.com>
2022-12-29 08:45:24 +0300

    Checkpoint before reorginzing the exercise_section

---
+++ gym_rate/lib/utils/exercise_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExercisesSection extends StatelessWidget {
+  const ExercisesSection({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
